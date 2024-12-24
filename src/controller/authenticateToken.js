f0d3d627635a8672086c37bea464631f2a94cfab
Ayoub <118635029+EAP44@users.noreply.github.com>
2024-12-24 17:24:36 +0100

    Update authenticateToken.js

--- src/controller/authenticateToken.js
+++ src/controller/authenticateToken.js
@@ -1,13 +1,15 @@
-const  jwt  = require('jsonwebtoken');
 require('dotenv').config();
-const SECRET_KEY = process.env.SECRET_KEY ;
+const jwt = require('jsonwebtoken');
+const SECRET_KEY = process.env.SECRET_KEY;
 
 function authenticateToken(req, res, next) {
-  const token = req.body.token;
-  console.log(token);
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1]; // Extract Bearer token
+
   if (!token) {
     return res.status(401).send("Token d'authentification manquant");
   }
+
   jwt.verify(token, SECRET_KEY, (err, user) => {
     if (err) {
       return res.status(403).send("Token invalide");
