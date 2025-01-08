e0646b661b1ae124c935af7ab5f8cac7e8953a3f
Ayoub <118635029+EAP44@users.noreply.github.com>
2025-01-08 14:14:45 +0100

    Update authenticateToken.js

--- src/controller/authenticateToken.js
+++ src/controller/authenticateToken.js
@@ -4,7 +4,7 @@ const SECRET_KEY = process.env.SECRET_KEY;
 
 function authenticateToken(req, res, next) {
   const authHeader = req.headers['authorization'];
-  const token = authHeader && authHeader.split(' ')[1]; // Extract Bearer token
+  const token = authHeader && authHeader.split(' ')[1];
 
   if (!token) {
     return res.status(401).send("Token d'authentification manquant");
